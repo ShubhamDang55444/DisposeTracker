1071b740ee46124c102a6859a292b7046358c05f
Dang <shubham.dang@siemens.com>
2020-04-24 22:32:57 +0530

    post operation

--- src/app/LogfileBrowser/logfilebrowser.component.ts
+++ src/app/LogfileBrowser/logfilebrowser.component.ts
@@ -1,95 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {HttpClientModule, HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { IReportParserData } from './reportParserData';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
-@Component ({
-    templateUrl:'./logfilebrowser.component.html',
-    styleUrls: ['./logfilebrowser.component.css']
+@Component({
+  templateUrl: './logfilebrowser.component.html',
+  styleUrls: ['./logfilebrowser.component.css'],
 })
-export class LogFileBrowserComponent implements OnInit
-{
-     private gridApi;
-     private gridColumnApi;  
-     columnDefs;
-      sortingOrder;
-    file:File = null;
-    reportParserDatUrl= 'http://localhost:3000/file';
-    reportParserData:string[];
-    filteredParserData:string[];
-  
-    search:string  ;
-    ngOnInit(): void {
-        // this.http.get(this.reportParserDatUrl).subscribe(res=> 
-        //     {
-        //         this.reportParserData= res as string[];
-        //         this.filteredParserData= this.reportParserData;
-        //         console.log(this.reportParserData);
-        //     }),
-        //     (err: HttpErrorResponse) => {
-        //       console.log (err.message);
-        //     }
+export class LogFileBrowserComponent implements OnInit {
+  private gridApi;
+  private gridColumnApi;
+  columnDefs;
+  sortingOrder;
+  errorMessage = '';
+  loading = true;
+  file: File = null;
 
-        
+  fileName: string = 'Enter Portal Profiler Log file To Proceed';
+  reportParserDatUrl = 'http://localhost:3000/file';
+  reportParserData: string[];
+  filteredParserData: string[];
+
+  search: string;
+  ngOnInit(): void {}
+  constructor(private http: HttpClient) {
+    this.columnDefs = [
+      {
+        headerName: 'NAMES',
+        field: 'm_Name',
+        width: 800,
+      },
+      {
+        headerName: 'Count',
+        field: 'm_Count',
+        width: 100,
+      },
+    ];
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    // this.http.get(this.reportParserDatUrl).subscribe((res) => {
+    //   //     console.log(res);
+    //   params.api.setRowData(res);
+    // });
+  }
+
+  onFileSelected(event) {
+    // console.log("hi");
+    console.log(event);
+    this.file = event.target.files[0];
+    this.fileName = event.target.files[0].name;
+    console.log('selectedFiles: ' + this.fileName);
+  }
+
+  UploadUserFile() {
+    let formdata: FormData = new FormData();
+    formdata.append('file', this.file, this.file.name);
+
+    const url = 'http://localhost:3000/api/file';
+
+    this.http
+      .post(url, formdata)
+
+      .subscribe((res) => {
+        console.log(res);
+        let newData = res;
+        this.gridApi.setRowData(newData),
+          (error) => {
+            //Error callback
+            console.error('error caught in component');
+          };
+      });
+  }
+
+  private ErrorHandler(errorResponse: HttpErrorResponse) {
+    if (errorResponse instanceof ErrorEvent) {
+      console.error('Client Side Error', errorResponse.error.message);
+    } else {
+      console.error('Server Side Error', errorResponse);
     }
-     constructor(private http:HttpClient){
-        this.columnDefs=[
-          {
-            headerName:"NAMES",
-            field:"m_Name",
-            width:800,
-           
-            
-          },
-          {
-            headerName:"Count",
-            field:"m_Count",
-            width:100
-          },
-        ]
-     }
-
-     onGridReady(params)
-     {
-       this.gridApi=params.api;
-       this.gridColumnApi=params.columnApi;
-     let dataValue= [{"m_Name":"DANG","m_Count":"40"}]
-        this.http.get(this.reportParserDatUrl)
-       .subscribe(res=>
-        {
-      //     console.log(res);
-          params.api.setRowData(res);
-         })
-    // params.api.setRowData(dataValue); 
-      }
-     
-    
-    onFileSelected(event)
-    {        
-       // console.log("hi");
-       console.log(event);
-        //console.log("by");
-      this.file= event.target.files[0];
-      
-    }
-    UploadUserFile()
-    {
-       let formdata:FormData=new FormData();
-       formdata.append('logFile',this.file,this.file.name)
-       const url='';
-
-       this.http.post(url,formdata).subscribe((res)=> console.log(res));
-    }
-    // performFilter(filterby:string):string[]
-    //   {
-    //         filterby=filterby.toLowerCase();
-    //         return this.reportParserData.filter((ppnode:string[]) => ppnode.name.toLowerCase().indexOf(filterby)!== -1
-    //         );
-    //   } 
-    
-
-  
+    return throwError('please try again');
+  }
 }
